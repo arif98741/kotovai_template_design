e9cbf968c0f004002f6432471e43a9104f6b61ee
Ariful <your_email@abc.com>
2021-09-13 02:17:02 +0600

    change

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 AOS.init();
 $('.intro').typeIt({
     whatToType: "Kotovai.com is a online platform for showing latest mobile specifications, review and prices. You can easily find your desired phones price and its prices So why not start finding?",
-    typeSpeed: 60
+    typeSpeed: 30
 });
