970a2b675b4e5f26cead41a8e95a1a57035f48b1
Ariful <your_email@abc.com>
2021-09-23 02:31:12 +0600

    chane

--- js/main.js
+++ js/main.js
@@ -3,3 +3,8 @@ $('.intro').typeIt({
     whatToType: "Kotovai.com is a online platform for showing latest mobile specifications, review and prices. You can easily find your desired phones price and its prices So why not start finding?",
     typeSpeed: 30
 });
+
+//lazy load
+var lazyLoadInstance = new LazyLoad({
+    // Your custom settings go here
+});
